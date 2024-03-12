a18c9048d51e0387ac4c768303889d50e6d111ca
Matias Cancina <cancinamatias@gmail.com>
2024-03-12 17:29:38 -0300

    home initialized

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,31 @@
+import { NavBar } from "@/components/NavBar/NavBar";
+
 export default function Home() {
-  return <div>eCommerce Coderhouse</div>;
+  return (
+    <div>
+      <NavBar />
+      <text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nam
+        iste nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum
+        eos quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Possimus nam iste nulla id sequi
+        exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
+        porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Possimus nam iste nulla id sequi exercitationem at,
+        culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
+        Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Possimus nam iste nulla id sequi exercitationem at, culpa ex qui,
+        corrupti ut earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Possimus nam iste
+        nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum eos
+        quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Possimus nam iste nulla id sequi
+        exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
+        porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Possimus nam iste nulla id sequi exercitationem at,
+        culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
+        Eveniet!
+      </text>
+    </div>
+  );
 }
