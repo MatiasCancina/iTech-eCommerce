c80db649728e98aace73472bff07c1ac05f15517
Matias Cancina <cancinamatias@gmail.com>
2024-03-13 18:14:23 -0300

    add Link tag

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -3,23 +3,23 @@ import Link from "next/link";
 export const NavBar = () => {
   return (
     <div className="flex items-center justify-between bg-gray-800 py-6 px-40">
-      <a className="text-white font-bold text-xl">eCommerce Coderhouse</a>
+      <span className="text-white font-bold text-xl">eCommerce Coderhouse</span>
       <nav className="flex justify-between gap-2">
-        <a href="#" className="text-base text-slate-100 p-3 ">
+        <Link href="#" className="text-base text-slate-100 p-3 ">
           TIENDA
-        </a>
-        <a href="#" className="text-base text-slate-100 p-3 ">
+        </Link>
+        <Link href="#" className="text-base text-slate-100 p-3 ">
           NOSOTROS
-        </a>
-        <a href="#" className="text-base text-slate-100 p-3 ">
+        </Link>
+        <Link href="#" className="text-base text-slate-100 p-3 ">
           3
-        </a>
-        <a href="#" className="text-base text-slate-100 p-3 ">
+        </Link>
+        <Link href="#" className="text-base text-slate-100 p-3 ">
           4
-        </a>
-        <a href="#" className="text-base text-slate-100 p-3 ">
+        </Link>
+        <Link href="#" className="text-base text-slate-100 p-3 ">
           CARRITO
-        </a>
+        </Link>
       </nav>
     </div>
   );
