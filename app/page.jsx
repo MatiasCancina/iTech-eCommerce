58abf800663bb5887836be053b7a107948a0d135
Matias Cancina <cancinamatias@gmail.com>
2024-03-13 18:01:52 -0300

    add structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,57 @@
+import Footer from "@/components/Footer/Footer";
 import { NavBar } from "@/components/NavBar/NavBar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="space-y-3">
       <NavBar />
-      <text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nam
-        iste nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum
-        eos quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Possimus nam iste nulla id sequi
-        exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
-        porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Possimus nam iste nulla id sequi exercitationem at,
-        culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
-        Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Possimus nam iste nulla id sequi exercitationem at, culpa ex qui,
-        corrupti ut earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Possimus nam iste
-        nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum eos
-        quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Possimus nam iste nulla id sequi
-        exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
-        porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Possimus nam iste nulla id sequi exercitationem at,
-        culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
-        Eveniet!
-      </text>
+      <div className="py-10 lg:px-96">
+        <div className="bg-white rounded-xl p-5">
+          <text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            nam iste nulla id sequi exercitationem at, culpa ex qui, corrupti ut
+            earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Possimus nam iste nulla id
+            sequi exercitationem at, culpa ex qui, corrupti ut earum eos quia
+            vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Possimus nam iste nulla id sequi
+            exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
+            porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Possimus nam iste nulla id sequi exercitationem
+            at, culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam
+            sunt. Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Possimus nam iste nulla id sequi exercitationem at, culpa ex
+            qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
+            Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Possimus nam iste nulla id sequi exercitationem at, culpa ex qui,
+            corrupti ut earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Possimus nam iste
+            nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum
+            eos quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Possimus nam iste nulla id sequi
+            exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
+            porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Possimus nam iste nulla id sequi exercitationem
+            at, culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam
+            sunt. Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Possimus nam iste nulla id sequi exercitationem at, culpa ex
+            qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
+            Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Possimus nam iste nulla id sequi exercitationem at, culpa ex qui,
+            corrupti ut earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Possimus nam iste
+            nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum
+            eos quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Possimus nam iste nulla id sequi
+            exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
+            porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Possimus nam iste nulla id sequi exercitationem
+            at, culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam
+            sunt.
+          </text>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
