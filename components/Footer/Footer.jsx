676234d0341c1146d8b759e2a246d40e79fd7c67
Matias Cancina <cancinamatias@gmail.com>
2024-03-13 18:14:45 -0300

    set boxes padding

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,29 +3,30 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full bottom-0 bg-white flex flex-col justify-center items-center px-96">
+
       {/*footer boxes*/}
       <div className="flex py-10 space-x-10">
-        <div className="flex flex-col justify-center items-center text-center w-full border border-gray-200 p-3 px-20 rounded-md">
-          <text className="font-medium text-slate-600">
+        <div className="flex flex-col justify-center items-center text-center w-1/2 border border-gray-200 p-3 px-28 rounded-md">
+          <span className="font-medium text-slate-600">
             Botón de arrepentimientio
-          </text>
-          <text className="text-sm items-center text-blue-500 cursor-pointer">
+          </span>
+          <span className="text-sm items-center text-blue-500 cursor-pointer">
             Cancelar compra
-          </text>
-          <text className="text-sm text-blue-500 cursor-pointer">
+          </span>
+          <span className="text-sm text-blue-500 cursor-pointer">
             Cancelar seguro y/o garantía
-          </text>
+          </span>
         </div>
-        <div className="flex flex-col justify-center items-center text-center w-full border border-gray-200 p-3 px-20 rounded-md">
-          <text className="flex justify-center items-center font-medium text-slate-600">
+        <div className="flex flex-col justify-center items-center text-center w-1/2 border border-gray-200 p-3 px-28 rounded-md">
+          <span className="flex justify-center items-center font-medium text-slate-600">
             Conocé las normas que aplican cuando comprás
-          </text>
-          <text className="text-sm text-blue-500 cursor-pointer">
+          </span>
+          <span className="text-sm text-blue-500 cursor-pointer">
             Ver contratos de adhesión - Ley N.º 24.240 de Defensa del Consumidor
-          </text>
+          </span>
         </div>
       </div>
-      <br />
+      
       <div className="border border-t-1 border-gray-200 w-screen flex items-center justify-center">
         contact info
       </div>
