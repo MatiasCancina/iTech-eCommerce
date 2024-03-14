56981d2e7a99bf07d932bcd1929451667df100de
Matias Cancina <cancinamatias@gmail.com>
2024-03-14 18:49:07 -0300

    home estructure

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,11 @@ import { NavBar } from "@/components/NavBar/NavBar";
 
 export default function Home() {
   return (
-    <div className="space-y-3">
+    <div>
       <NavBar />
-      <div className="py-10 lg:px-96">
-        <div className="bg-white rounded-xl p-5">
-          <text>
+      <div className="py-10 lg:px-96 flex items-center justify-center">
+        <div className="bg-white rounded-xl p-5 w-11/12">
+          <text className="z-50 relative">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
             nam iste nulla id sequi exercitationem at, culpa ex qui, corrupti ut
             earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor
