f702d35597beb341f180d806c533bf870a269c1d
Matias Cancina <cancinamatias@gmail.com>
2024-03-15 14:46:39 -0300

    responsive

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <NavBar />
-      <div className="py-10 lg:px-96 flex items-center justify-center">
+      <div className="py-10 md:px-32 lg:px-40 xl:px-52 2xl:px-96 flex items-center justify-center">
         <div className="bg-white rounded-xl p-5 w-11/12 lg:w-full">
           <text>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
