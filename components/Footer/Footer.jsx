5645fa4e43c1bd5a5a53b3f1546f8a3a3f05259d
Matias Cancina <cancinamatias@gmail.com>
2024-03-15 14:46:28 -0300

    footer boxes responsive

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,11 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full bottom-0 bg-white flex flex-col justify-center items-center px-96">
-
+    <div className="w-full bottom-0 bg-white flex flex-col justify-center items-center">
       {/*footer boxes*/}
-      <div className="flex py-10 space-x-10">
-        <div className="flex flex-col justify-center items-center text-center w-1/2 border border-gray-200 p-3 px-28 rounded-md">
+      <div className="flex flex-col md:flex-row justify-center items-center py-10 space-y-3 md:space-y-0 md:space-x-4  xl:space-x-5 2xl:space-x-10">
+        <div className="flex flex-col justify-center items-center text-center w-11/12 lg:w-1/2 border border-gray-200 p-3 2xl:px-36 py-6 sm:py-2 md:py-6 xl:py-7 2xl:py-3 rounded-md">
           <span className="font-medium text-slate-600">
             Botón de arrepentimientio
           </span>
@@ -17,7 +16,7 @@ const Footer = () => {
             Cancelar seguro y/o garantía
           </span>
         </div>
-        <div className="flex flex-col justify-center items-center text-center w-1/2 border border-gray-200 p-3 px-28 rounded-md">
+        <div className="flex flex-col justify-center items-center text-center w-11/12 lg:w-1/2 border border-gray-200 p-3 xl:px-16 rounded-md">
           <span className="flex justify-center items-center font-medium text-slate-600">
             Conocé las normas que aplican cuando comprás
           </span>
@@ -26,8 +25,8 @@ const Footer = () => {
           </span>
         </div>
       </div>
-      
-      <div className="border border-t-1 border-gray-200 w-screen flex items-center justify-center">
+
+      <div className="hidden border border-t-1 border-gray-200 w-screen md:flex items-center justify-center">
         contact info
       </div>
     </div>
