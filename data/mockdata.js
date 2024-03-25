c6aaa30205e3825195b82a3298bcd0a2d9945376
Matias Cancina <cancinamatias@gmail.com>
2024-03-25 17:28:03 -0300

    add product data

---
+++ data/mockdata.js
@@ -0,0 +1,64 @@
+import imgProductRandom from "@/public/imgProductRandom.png";
+
+export const mockData = [
+  {
+    title: "monitor 1",
+    description:
+      "Un monitor de alta resolución con un diseño elegante y moderno, ofrece colores vibrantes y un excelente contraste para una experiencia visual inmersiva. Además, cuenta con múltiples puertos de conectividad para adaptarse a diversas necesidades de uso.",
+    inStock: 100,
+    price: 200,
+    id: 1,
+    image: imgProductRandom,
+    type: "monitor",
+  },
+  {
+    title: "monitor 2",
+    description:
+      "Un monitor de alta resolución con un diseño elegante y moderno, ofrece colores vibrantes y un excelente contraste para una experiencia visual inmersiva. Además, cuenta con múltiples puertos de conectividad para adaptarse a diversas necesidades de uso.",
+    inStock: 100,
+    price: 300,
+    id: 2,
+    image: imgProductRandom,
+    type: "monitor",
+  },
+  {
+    title: "mouse 1",
+    description:
+      "Un mouse ergonómico y preciso que se adapta cómodamente a la mano del usuario, proporcionando un desplazamiento suave y preciso en diversas superficies. Con botones programables para personalizar las funciones según las preferencias del usuario y una conectividad inalámbrica confiable para una experiencia libre de cables.",
+    inStock: 100,
+    price: 100,
+    id: 3,
+    image: imgProductRandom,
+    type: "mouse",
+  },
+  {
+    title: "mouse 2",
+    description:
+      "Un mouse ergonómico y preciso que se adapta cómodamente a la mano del usuario, proporcionando un desplazamiento suave y preciso en diversas superficies. Con botones programables para personalizar las funciones según las preferencias del usuario y una conectividad inalámbrica confiable para una experiencia libre de cables.",
+    inStock: 100,
+    price: 200,
+    id: 4,
+    image: imgProductRandom,
+    type: "mouse",
+  },
+  {
+    title: "keyboard 1",
+    description:
+      "Un teclado de diseño delgado y elegante con teclas de pulsación suave que proporcionan una experiencia de escritura cómoda y silenciosa. Equipado con retroiluminación ajustable para facilitar la escritura en entornos con poca luz y teclas multimedia para un acceso rápido a funciones adicionales. Conectividad versátil que permite su uso con una amplia gama de dispositivos.",
+    inStock: 100,
+    price: 400,
+    id: 5,
+    image: imgProductRandom,
+    type: "keyboard",
+  },
+  {
+    title: "keyboard 2",
+    description:
+      "Un teclado de diseño delgado y elegante con teclas de pulsación suave que proporcionan una experiencia de escritura cómoda y silenciosa. Equipado con retroiluminación ajustable para facilitar la escritura en entornos con poca luz y teclas multimedia para un acceso rápido a funciones adicionales. Conectividad versátil que permite su uso con una amplia gama de dispositivos.",
+    inStock: 100,
+    price: 500,
+    id: 6,
+    image: imgProductRandom,
+    type: "keyboard",
+  },
+];
