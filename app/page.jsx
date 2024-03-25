7cd5620457aa33b5c300efad6ff8f933d820925a
Matias Cancina <cancinamatias@gmail.com>
2024-03-25 19:31:41 -0300

    add Products component

--- app/page.jsx
+++ app/page.jsx
@@ -1,48 +1,10 @@
+import Products from "./products/[category]/page";
+
 export default function Home() {
   return (
     <div className="py-10 md:px-32 lg:px-40 xl:px-52 2xl:px-96 flex items-center justify-center">
       <div className="bg-white rounded-xl p-5 w-11/12 lg:w-full">
-        <text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nam
-          iste nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum
-          eos quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Possimus nam iste nulla id sequi
-          exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
-          porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Possimus nam iste nulla id sequi exercitationem at,
-          culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
-          Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Possimus nam iste nulla id sequi exercitationem at, culpa ex qui,
-          corrupti ut earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Possimus nam iste
-          nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum eos
-          quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Possimus nam iste nulla id sequi
-          exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
-          porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Possimus nam iste nulla id sequi exercitationem at,
-          culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
-          Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Possimus nam iste nulla id sequi exercitationem at, culpa ex qui,
-          corrupti ut earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Possimus nam iste
-          nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum eos
-          quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Possimus nam iste nulla id sequi
-          exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
-          porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Possimus nam iste nulla id sequi exercitationem at,
-          culpa ex qui, corrupti ut earum eos quia vel aut porro aliquam sunt.
-          Eveniet!Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Possimus nam iste nulla id sequi exercitationem at, culpa ex qui,
-          corrupti ut earum eos quia vel aut porro aliquam sunt. Eveniet!Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Possimus nam iste
-          nulla id sequi exercitationem at, culpa ex qui, corrupti ut earum eos
-          quia vel aut porro aliquam sunt. Eveniet!Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Possimus nam iste nulla id sequi
-          exercitationem at, culpa ex qui, corrupti ut earum eos quia vel aut
-          porro aliquam sunt.
-        </text>
+        <Products />
       </div>
     </div>
   );
