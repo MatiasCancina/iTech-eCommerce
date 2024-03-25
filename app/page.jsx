a614faaf4a7838ec237082d8051f5827c8b0b74c
Matias Cancina <cancinamatias@gmail.com>
2024-03-25 19:37:14 -0300

    add Link to /products

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import Products from "./products/[category]/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="py-10 md:px-32 lg:px-40 xl:px-52 2xl:px-96 flex items-center justify-center">
       <div className="bg-white rounded-xl p-5 w-11/12 lg:w-full">
-        <Products />
+        <Link href={'/products/all'}>ir a products</Link>
       </div>
     </div>
   );
