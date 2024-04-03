f45a49b437b6c9821530a01d5923b7a8d2c414bd
Matias Cancina <cancinamatias@gmail.com>
2024-04-03 14:58:25 -0300

    delete unnecessary lines

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,9 @@
 import ProductsAll from "@/components/ProductsAll/ProductsAll";
-import Link from "next/link";
-// import Products from "./[category]/page";
 
 export default function Home() {
   return (
     <div className="py-10 md:px-32 lg:px-40 xl:px-52 2xl:px-96 flex items-center justify-center">
       <div className="bg-white rounded-xl p-5 w-11/12 lg:w-full">
-        {/* <Link href={'/products/all'}>ir a products</Link> */}
         <ProductsAll />
       </div>
     </div>
--- components/ProductsAll/ProductsAll.jsx
+++ components/ProductsAll/ProductsAll.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import ProductList from "@/components/products/ProductsList";
 import CategoriesMenu from "../products/CategoriesMenu";
 import ProductsList from "@/components/products/ProductsList";
 
