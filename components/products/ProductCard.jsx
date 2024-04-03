9d0c4583b6337b94240824f08feefb372f821525
Matias Cancina <cancinamatias@gmail.com>
2024-04-03 15:28:57 -0300

    card style

--- components/products/ProductCard.jsx
+++ components/products/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({ item }) => {
     <article className="shadow-lg rounded m-3 hover:translate-y-1 hover:shadow-2xl transition">
       <Link
         href={`/detail/${item.id}`}
-        className="flex flex-col rounded p-3 border "
+        className="flex flex-col rounded py-3 border "
       >
         <div className="border-b border-gray-200 mb-2">
           <Image
@@ -19,7 +19,7 @@ const ProductCard = ({ item }) => {
           />
         </div>
 
-        <div className="space-y-2">
+        <div className="space-y-2 px-3">
           <h4>{item.title}</h4>
           <p className="text-2xl">${item.price}</p>
         </div>
