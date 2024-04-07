d4d2e3a99b4881fa09671462d0eb2ca0d52f049c
Matias Cancina <cancinamatias@gmail.com>
2024-04-07 17:55:50 -0300

    add try catch

--- components/products/ProductsList.jsx
+++ components/products/ProductsList.jsx
@@ -1,21 +1,26 @@
-import { mockData } from "@/data/mockdata";
-import React from "react";
 import ProductCard from "./ProductCard";
 
 const ProductsList = async ({ category }) => {
-  const items = await fetch(`http://localhost:3000//api/products/${category}`, {
-    cache: "force-cache",
-  }).then((r) => r.json());
+  try {
+    const items = await fetch(
+      `http://localhost:3000/api/products/${category}`,
+      {
+        cache: "force-cache",
+      }
+    ).then((r) => r.json());
 
-  return (
-    <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
-      {items.map((item) => (
-        <div className="w-full" key={item.id}>
-          <ProductCard key={item.id} item={item} />
-        </div>
-      ))}
-    </section>
-  );
+    return (
+      <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
+        {items.map((item) => (
+          <div className="w-full" key={item.id}>
+            <ProductCard key={item.id} item={item} />
+          </div>
+        ))}
+      </section>
+    );
+  } catch (error) {
+    return <div>Error fetching data</div>;
+  }
 };
 
 export default ProductsList;
