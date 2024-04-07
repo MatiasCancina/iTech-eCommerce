522314b5f50e5515ee13e4f9d01b740dabb9e794
Matias Cancina <cancinamatias@gmail.com>
2024-04-07 17:42:10 -0300

    add revalidatePath

--- app/api/products/[category]/route.js
+++ app/api/products/[category]/route.js
@@ -1,9 +1,11 @@
 import { mockData } from "@/data/mockdata";
 import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
 const sleep = (timer) => {
   return new Promise((resolve) => setTimeout(resolve, timer));
 };
+
 export async function GET(request, { params }) {
   const { category } = params;
 
@@ -13,6 +15,8 @@ export async function GET(request, { params }) {
       : mockData.filter((item) => item.type === category);
 
   await sleep(1000);
+
+  revalidatePath("/");
 
   return NextResponse.json(data);
 }
