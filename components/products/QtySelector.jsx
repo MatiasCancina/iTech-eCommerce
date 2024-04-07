d96a40a581ee3b5314e712f9851e224540960f4f
Matias Cancina <cancinamatias@gmail.com>
2024-04-07 17:43:48 -0300

    add Buy Now link to /cart

--- components/products/QtySelector.jsx
+++ components/products/QtySelector.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import Counter from "../ui/Counter";
+import Link from "next/link";
 
 const QtySelector = ({ item }) => {
   const [quantity, setQuantity] = useState(1);
@@ -16,12 +17,13 @@ const QtySelector = ({ item }) => {
     <div className="flex flex-col gap-3 mt-6">
       <Counter max={item.inStock} counter={quantity} setCounter={setQuantity} />
       <div className="flex flex-col "></div>
-      <button
-        className="bg-gray-800 text-white rounded py-3"
+      <Link
+        href={"/cart"}
+        className="bg-gray-800 text-white rounded py-3 flex items-center justify-center"
         onClick={handleAdd}
       >
         Buy now
-      </button>
+      </Link>
       <button
         className="bg-slate-300 text-gray-800 font-medium rounded py-3"
         onClick={handleAdd}
