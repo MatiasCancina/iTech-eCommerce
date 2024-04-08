3731a95d0e3f57edcf5de99f5ac8472fe821213e
Matias Cancina <cancinamatias@gmail.com>
2024-04-08 14:15:56 -0300

    trying fetching data

--- components/products/ProductsList.jsx
+++ components/products/ProductsList.jsx
@@ -1,26 +1,19 @@
 import ProductCard from "./ProductCard";
 
 const ProductsList = async ({ category }) => {
-  try {
-    const items = await fetch(
-      `http://localhost:3000/api/products/${category}`,
-      {
-        cache: "force-cache",
-      }
-    ).then((r) => r.json());
+  const items = await fetch(`http://localhost:3000//api/products/${category}`, {
+    cache: "force-cache",
+  }).then((r) => r.json());
 
-    return (
-      <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
-        {items.map((item) => (
-          <div className="w-full" key={item.id}>
-            <ProductCard key={item.id} item={item} />
-          </div>
-        ))}
-      </section>
-    );
-  } catch (error) {
-    return <div>Error fetching data</div>;
-  }
+  return (
+    <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
+      {items.map((item) => (
+        <div className="w-full" key={item.id}>
+          <ProductCard key={item.id} item={item} />
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default ProductsList;
