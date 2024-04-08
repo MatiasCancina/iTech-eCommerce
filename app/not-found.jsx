3e4050b3cc56b1e548404de74ac1c1fdd6821453
Matias Cancina <cancinamatias@gmail.com>
2024-04-08 19:37:34 -0300

    styles

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,28 +1,19 @@
-"use client";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React from "react";
 
 export default function NotFound() {
-  const router = useRouter();
-
-  const handleGoBack = () => {
-    router.back();
-  };
-
   return (
     <>
       <di className="flex justify-start items-center flex-col py-52 select-none ">
         <div className="py-2">
-          <h1 className="text-9xl text-slate-600 font-thin">SORRY</h1>
-          <p className="text-5xl text-slate-600 font-light">
+          <h1 className="text-9xl text-gray font-thin">SORRY</h1>
+          <p className="text-5xl text-gray font-light">
             we couldn´t find that page
           </p>
           <div className="flex space-x-3 mt-2">
-            <p className="text-2xl text-slate-600 font-medium ">
+            <p className="text-2xl text-gray font-medium ">
               Try searching or go
             </p>
-            <Link href={"/"} className="text-2xl text-blue-800 font-medium">
+            <Link href={"/"} className="text-2xl text-blueLink font-medium">
               iTech´s home page
             </Link>
           </div>
