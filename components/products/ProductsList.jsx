58f749c33fed3d78e9da469c556c470f3362da88
Matias Cancina <cancinamatias@gmail.com>
2024-04-08 14:22:29 -0300

    loader

--- components/products/ProductsList.jsx
+++ components/products/ProductsList.jsx
@@ -32,6 +32,10 @@ const ProductsList = ({ category }) => {
     return <div>{error}</div>;
   }
 
+  if(!items){
+    return <div>Loading...</div>;
+  }
+
   return (
     <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
       {items.map((item) => (
