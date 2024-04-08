724747ee6a2941098fea3a331c658598b4c1ed12
Matias Cancina <cancinamatias@gmail.com>
2024-04-08 14:20:36 -0300

    2nd try fetching data

--- components/products/ProductsList.jsx
+++ components/products/ProductsList.jsx
@@ -1,9 +1,36 @@
+"use client";
+import { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 
-const ProductsList = async ({ category }) => {
-  const items = await fetch(`http://localhost:3000//api/products/${category}`, {
-    cache: "force-cache",
-  }).then((r) => r.json());
+const ProductsList = ({ category }) => {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/products/${category}`,
+          {
+            cache: "force-cache",
+            next: {
+              revalidate: 60,
+            },
+          }
+        );
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        setError("Error fetching data");
+      }
+    };
+
+    fetchData();
+  }, [category]);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
