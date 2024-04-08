ffe9b49ab97c18e594b29b31890485d1af47f0e2
Matias Cancina <cancinamatias@gmail.com>
2024-04-08 15:17:00 -0300

    add sweetalert

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 const ContactForm = () => {
   const [values, setValues] = useState({
@@ -19,13 +20,26 @@ const ContactForm = () => {
       method: "POST",
       body: JSON.stringify(values),
     });
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      iconColor: "#457b9d",
+      title: "Message sent!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   };
 
   return (
     <div className=" my-16 px-3 sm:px-20 lg:px-40 xl:px-52 2xl:px-96 select-none">
-      <h1 className="font-semibold text-4xl border-b border-gray text-cyan">CONTACT US</h1>
+      <h1 className="font-semibold text-4xl border-b border-gray text-cyan">
+        CONTACT US
+      </h1>
       <div className="flex justify-center items-center">
-        <form onSubmit={handleSubmit} className="grid gap-4 xl:w-3/4 my-6 sm:my-3">
+        <form
+          onSubmit={handleSubmit}
+          className="grid gap-4 xl:w-3/4 my-6 sm:my-3"
+        >
           <div className="grid grid-cols-2 gap-4">
             <input
               type="text"
@@ -60,7 +74,10 @@ const ContactForm = () => {
           />
 
           <div className="flex justify-end items-end">
-            <button type="submit" className="bg-cyan py-3 px-6 sm:px-10 text-white shadow-md">
+            <button
+              type="submit"
+              className="bg-cyan py-3 px-6 sm:px-10 text-white shadow-md"
+            >
               Submit
             </button>
           </div>
