db243692cd884e9143c834e0f9fe244d48b58692
Matias Cancina <cancinamatias@gmail.com>
2024-04-11 16:35:11 -0300

    firestore

--- app/api/product/[id]/route.js
+++ app/api/product/[id]/route.js
@@ -1,16 +1,13 @@
-import { mockData } from "@/data/mockdata";
+import { db } from "@/firebase/config";
+import { doc, getDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
-
-const sleep = (timer) => {
-  return new Promise((resolve) => setTimeout(resolve, timer));
-};
 
 export async function GET(_, { params }) {
   const { id } = params;
-  const data = mockData.find((p) => p.id === parseInt(id, 10));
 
-  await sleep(1000);
+  const docRef = doc(db, "products", id);
 
-  // Pasar el objeto data directamente a NextResponse.json()
-  return NextResponse.json(data);
+  const docSnapshot = await getDoc(docRef);
+
+  return NextResponse.json(docSnapshot.data());
 }
