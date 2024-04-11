40272e23de2b864787c54ae58195f413e264b504
Matias Cancina <cancinamatias@gmail.com>
2024-04-11 16:33:57 -0300

    trycatch no products error

--- components/products/ProductsList.jsx
+++ components/products/ProductsList.jsx
@@ -10,15 +10,21 @@ const ProductsList = async ({ category }) => {
 
   console.log("ITEMS", items);
 
-  return (
-    <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
-      {items.map((item) => (
-        <div className="w-full" key={item.id}>
-          <ProductCard key={item.id} item={item} />
-        </div>
-      ))}
-    </section>
-  );
+  try {
+    if (!items || !items.length) throw Error("No products yet");
+
+    return (
+      <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
+        {items.map((item) => (
+          <div className="w-full" key={item.id}>
+            <ProductCard key={item.id} item={item} />
+          </div>
+        ))}
+      </section>
+    );
+  } catch (error) {
+    return <p>No products yet</p>;
+  }
 };
 
 export default ProductsList;
