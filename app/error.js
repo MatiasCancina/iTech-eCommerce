7fad63681121eed9d82ceb8c5a9f4f7a0d5dfa73
Matias Cancina <cancinamatias@gmail.com>
2024-04-10 22:00:07 -0300

    delete comments

--- app/error.js
+++ app/error.js
@@ -1,16 +1,11 @@
-// Error.
-'use client'
-import { useEffect } from 'react';
-import ErrorUI from '../components/ui/Error.jsx';
+"use client";
+import { useEffect } from "react";
+import ErrorUI from "../components/ui/Error.jsx";
 
 export default function Error({ errorMessage, reset }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(errorMessage);
-
-    // Set a user-friendly error message
   }, [errorMessage]);
 
-  return (
-<ErrorUI/>  );
+  return <ErrorUI />;
 }
