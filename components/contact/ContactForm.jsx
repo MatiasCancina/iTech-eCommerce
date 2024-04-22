3fbef7d3254b5d4ad36c3804e77f55698e329f41
Matias Cancina <cancinamatias@gmail.com>
2024-04-22 14:27:39 -0300

    styles

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -31,7 +31,7 @@ const ContactForm = () => {
   };
 
   return (
-    <div className=" my-16 px-3 sm:px-20 lg:px-40 xl:px-52 2xl:px-96 select-none">
+    <div className="my-16 px-3 sm:px-20 lg:px-40 xl:px-52 2xl:px-96 select-none">
       <h1 className="font-semibold text-4xl border-b border-gray text-cyan">
         CONTACT US
       </h1>
