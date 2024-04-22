f60340774cc3380129a361a2e2218e4594ff08e9
Matias Cancina <cancinamatias@gmail.com>
2024-04-22 20:39:25 -0300

    logout btn

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,12 +1,16 @@
+import LogoutBtn from "@/components/admin/LogoutBtn";
 import ProductsTable from "@/components/admin/ProductsTable";
 import React from "react";
 
 const Admin = () => {
   return (
     <div className="my-16 px-3 sm:px-20 select-none">
-      <h2 className="font-semibold text-4xl border-b border-gray text-cyan">
-        Administration Panel
-      </h2>
+      <div className="flex justify-between items-center font-medium border-b border-gray">
+        <h2 className="font-semibold text-4xl  text-cyan">
+          Administration Panel
+        </h2>
+        <LogoutBtn />
+      </div>
       <ProductsTable />
     </div>
   );
