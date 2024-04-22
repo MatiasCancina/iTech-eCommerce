f9946ec9366eb7ce07f1c2ccc795593ee363b0df
Matias Cancina <cancinamatias@gmail.com>
2024-04-22 20:39:05 -0300

    auth context

---
+++ components/context/AuthContext.jsx
@@ -0,0 +1,68 @@
+"use client";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth, provider } from "@/firebase/config";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithPopup,
+} from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    logged: false,
+    email: null,
+    uid: null,
+  });
+
+  const registerUser = async (values) => {
+    await createUserWithEmailAndPassword(auth, values.email, values.password);
+  };
+
+  const loginUser = async (values) => {
+    await signInWithEmailAndPassword(auth, values.email, values.password);
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  const googleLogin = async () => {
+    await signInWithPopup(auth, provider);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+
+      if (user) {
+        setUser({
+          logged: true,
+          email: user.email,
+          uid: user.uid,
+        });
+      } else {
+        setUser({
+          logged: false,
+          email: null,
+          uid: null,
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, registerUser, loginUser, logout, googleLogin }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
