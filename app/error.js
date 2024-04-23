a89e39e79ef2fee3708c539ee9bb67627f404fc2
Matias Cancina <cancinamatias@gmail.com>
2024-04-23 15:06:12 -0300

    error page

--- app/error.js
+++ app/error.js
@@ -1,11 +1,24 @@
 "use client";
 import { useEffect } from "react";
-import ErrorUI from "../components/ui/Error.jsx";
 
 export default function Error({ errorMessage, reset }) {
   useEffect(() => {
     console.error(errorMessage);
   }, [errorMessage]);
 
-  return <ErrorUI />;
+  return (
+    <div className="flex justify-start items-center flex-col py-52 select-none ">
+      <div className="py-2">
+        <h1 className="text-9xl text-gray font-thin">SORRY</h1>
+        <p className="text-5xl text-gray font-light">
+          something go wrong
+        </p>
+        <div className="flex mt-2">
+          <button onClick={reset} className="text-2xl text-blueLink font-medium">
+            Try again
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
