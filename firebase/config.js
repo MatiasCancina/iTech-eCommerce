06dc591750db87b65c6d30166e147ab5f635063f
Matias Cancina <cancinamatias@gmail.com>
2024-04-23 15:41:13 -0300

    env

---
+++ firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: `${process.env.FIRABSE_API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`,
+  measurementId: `${process.env.MEASUREMENT_ID}`,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider()
