ba9f1c51230a9562c3c4bfa9feca4cd09b0e3c5a
Matias Cancina <cancinamatias@gmail.com>
2024-04-24 12:25:57 -0300

    products fetch hook

--- components/products/ProductsList.jsx
+++ components/products/ProductsList.jsx
@@ -1,32 +1,27 @@
+"use client";
+import useProductsFetch from "@/hooks/useProductsFetch";
 import ProductCard from "./ProductCard";
+import Loader from "../ui/Loader";
 
-const ProductsList = async ({ category }) => {
-  const items = await fetch(`http://localhost:3000/api/products/${category}`, {
-    cache: "force-cache",
-    next: {
-      revalidate: 30,
-    },
-  }).then((r) => r.json());
+const ProductsList = ({ category }) => {
+  const { items, loading } = useProductsFetch(category);
 
-  try {
-    if (!items || !items.length) throw Error("No products yet");
-
-    return (
-      <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
-        {items.map((item) => (
-          <div className="w-full" key={item.id}>
-            <ProductCard key={item.id} item={item} />
-          </div>
-        ))}
-      </section>
-    );
-  } catch (error) {
-    return (
-      <>
-        <p>No products yet</p>;
-      </>
-    );
+  if (loading) {
+    return <Loader />;
   }
+
+  return (
+    <>
+      <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full">
+        {items.length &&
+          items.map((item) => (
+            <div className="w-full" key={item.id}>
+              <ProductCard key={item.id} item={item} />
+            </div>
+          ))}
+      </section>
+    </>
+  );
 };
 
 export default ProductsList;
