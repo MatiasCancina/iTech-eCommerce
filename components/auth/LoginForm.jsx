38203a9ab237a94932e7c02245024895aaa68f41
Matias Cancina <cancinamatias@gmail.com>
2024-04-26 12:28:32 -0300

    styles

--- components/auth/LoginForm.jsx
+++ components/auth/LoginForm.jsx
@@ -1,6 +1,9 @@
 "use client";
 import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import { IoPersonOutline, IoPerson } from "react-icons/io5";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FcGoogle } from "react-icons/fc";
 
 const LoginForm = () => {
   const { registerUser, loginUser, googleLogin } = useAuthContext();
@@ -19,50 +22,64 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="fixed w-screen h-screen inset-0 z-10 flex justify-center items-center bg-darkBlue">
+    <div className="fixed inset-0 z-10 flex justify-center items-center backdrop-blur-xl px-3">
       <form
         onSubmit={handleSubmit}
-        className="bg-white py-4 px-6 rounded-xl max-w-md w-full"
+        className="bg-darkBlue py-4 px-6 rounded-xl max-w-md w-full space-y-4"
       >
-        <h2>Login</h2>
-        <input
-          type="email"
-          value={values.email}
-          required
-          placeholder="Your email"
-          className="p-2 rounded w-full border border-cyan block my-4"
-          name="email"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          value={values.password}
-          required
-          placeholder="Your password"
-          className="p-2 rounded w-full border border-cyan block my-4"
-          name="password"
-          onChange={handleChange}
-        />
-        <div  className="flex justify-between gap-4">
+        <div className="flex justify-center items-center ">
+          <IoPersonOutline className="text-9xl text-white border-2 rounded-full border-white p-4" />
+        </div>
 
-        <button className="bg-cyan py-3 px-6 sm:px-10 text-white shadow-md" 
-          onClick={() => loginUser(values)}
-        >
-          Log In
-        </button>
-        <button
-          className="bg-cyan py-3 px-6 sm:px-10 text-white shadow-md"
-          onClick={() => googleLogin()}
-          >
-          Log In with Google
-        </button>
-        <button
-          className="bg-cyan py-3 px-6 sm:px-10 text-white shadow-md"
-          onClick={() => registerUser(values)}
-          >
-          Register
-        </button>
+        <div className="flex space-x-2 items-center">
+          <IoPerson className="text-3xl text-white" />
+          <input
+            type="email"
+            value={values.email}
+            required
+            placeholder="Email"
+            className="p-2 rounded-e-lg w-full block"
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex space-x-2 items-center">
+          <RiLockPasswordFill className="text-3xl text-white" />
+          <input
+            type="password"
+            value={values.password}
+            required
+            placeholder="Password"
+            className="p-2 rounded-e-lg w-full block"
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex flex-col space-y-3">
+          <div className="flex justify-between">
+            <button
+              className="bg-white py-3 px-6 sm:px-10 text-darkBlue rounded-full"
+              onClick={() => loginUser(values)}
+            >
+              Login
+            </button>
+            <button
+              className="flex items-center bg-white py-3 px-6 text-darkBlue rounded-full"
+              onClick={() => googleLogin()}
+            >
+              <FcGoogle className="text-3xl" />
+              Login with Google
+            </button>
           </div>
+          <button
+            className="bg-white py-3 px-6  text-darkBlue rounded-full"
+            onClick={() => registerUser(values)}
+          >
+            Register
+          </button>
+        </div>
       </form>
     </div>
   );
