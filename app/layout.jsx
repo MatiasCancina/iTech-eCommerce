33c2317e4c30846966e36fd778b557054efe50b5
Matias Cancina <cancinamatias@gmail.com>
2024-04-29 13:25:29 -0300

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href={'../public/itech_logo.png'} />
+        <link rel="icon" href="../public/itech_logo.png" />
       </head>
       <body className={inter.className}>
         <AuthProvider>
