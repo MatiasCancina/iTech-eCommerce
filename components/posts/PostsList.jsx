7d74a0f90b921ceb3ccbb2a87fb1499bbf3ef2aa
Matias Cancina <cancinamatias@gmail.com>
2024-04-29 21:20:55 -0300

    rebuild

--- components/posts/PostsList.jsx
+++ components/posts/PostsList.jsx
@@ -1,23 +1,20 @@
 import { PostCard } from "./PostCard";
 
 export const PostsList = async () => {
-  // const posts = await fetch(`http://${process.env.VERCEL_URL}/api/posts`, {
-  //   cache: "no-store",
-  //   next: {
-  //     tags: ['posts'],
-  //   },
-  // }).then((r) => r.json());
+  const posts = await fetch(`http://${process.env.VERCEL_URL}/api/posts`, {
+    cache: "no-store",
+    next: {
+      tags: ['posts'],
+    },
+  }).then((r) => r.json());
 
   return (
-    // <div className="grid md:grid-cols-2 2xl:grid-cols-3 w-full lg:px-40 xl:px-62 2xl:px-52 py-6 select-none">
-    //   {posts.map((post) => (
-    //     <div className="w-full" key={post.id}>
-    //       <PostCard post={post} key={post.id} />
-    //     </div>
-    //   ))}
-    // </div>
-    <div>
-      hola
+    <div className="grid md:grid-cols-2 2xl:grid-cols-3 w-full lg:px-40 xl:px-62 2xl:px-52 py-6 select-none">
+      {posts.map((post) => (
+        <div className="w-full" key={post.id}>
+          <PostCard post={post} key={post.id} />
+        </div>
+      ))}
     </div>
   );
 };
