9cfd52946ce09f0b4da254e0bee62bbd976cdf8d
Matias Cancina <cancinamatias@gmail.com>
2024-04-29 21:44:39 -0300

    trying deploy

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -4,13 +4,13 @@ import { db } from "@/firebase/config";
 import { revalidateTag } from "next/cache";
 
 export async function GET() {
-  const productsRef = collection(db, "posts");
+//   const productsRef = collection(db, "posts");
 
-  const querySnapshot = await getDocs(productsRef);
+//   const querySnapshot = await getDocs(productsRef);
 
-  const docs = querySnapshot.docs.map((doc) => doc.data());
+//   const docs = querySnapshot.docs.map((doc) => doc.data());
 
-  revalidateTag("posts");
+//   revalidateTag("posts");
 
-  return NextResponse.json(docs);
+//   return NextResponse.json(docs);
 }
