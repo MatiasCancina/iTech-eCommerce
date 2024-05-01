767443143a0b73250957c74eebedc8aab0e1a955
Matias Cancina <cancinamatias@gmail.com>
2024-05-01 20:27:35 -0300

    trying fix fetch products error

--- components/products/ProductsList.jsx
+++ components/products/ProductsList.jsx
@@ -1,9 +1,8 @@
 import ProductCard from "./ProductCard";
 
 const ProductsList = async ({ category }) => {
-  try {
-    const items = await fetch(
-      `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products/${category}`,
+  const items = await fetch(
+    `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products/${category}`,
     {
       cache: "force-cache",
       next: {
@@ -11,7 +10,9 @@ const ProductsList = async ({ category }) => {
       },
     }
   ).then((r) => r.json());
-    if (!items || !items.length || items === "undefined") {
+  
+  try {
+  if (!items || !items.length || items === "undefined") {
       console.log("No products yet");
     }
 
