e4dc1ec1d988b85b3b8b4f52c6a10949f8111c7a
Matias Cancina <cancinamatias@gmail.com>
2024-05-02 16:50:04 -0300

    client form order

--- components/cart/CartForm.jsx
+++ components/cart/CartForm.jsx
@@ -1,38 +1,10 @@
 "use client";
 import CartItem from "@/components/cart/CartItem";
 import { useCartContext } from "@/components/context/CartContext";
-import { useState } from "react";
-import Swal from "sweetalert2";
+import ClientForm from "./ClientForm";
 
 const CartForm = () => {
   const { cart, totalPrice } = useCartContext();
-  const [values, setValues] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    cart,
-  });
-
-  const handleChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
-    await fetch(`http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/cart`, {
-      method: "POST",
-      body: JSON.stringify(values),
-    });
-    Swal.fire({
-      position: "top-end",
-      icon: "success",
-      iconColor: "#457b9d",
-      title: "Purchase done!",
-      showConfirmButton: false,
-      timer: 1500,
-    });
-  };
 
   return (
     <main className="my-16 px-3 sm:px-20 lg:px-40 xl:px-52 2xl:px-96 select-none">
@@ -46,41 +18,7 @@ const CartForm = () => {
         <h2 className="text-black font-semibold text-2xl pb-3 my-3 border-b border-borderGray">
           Total: $ {totalPrice()}
         </h2>
-        <form className="grid gap-4 xl:w-1/2 my-6 pb-4" onSubmit={handleSubmit}>
-          <div className="grid grid-cols-2 gap-4">
-            <input
-              type="text"
-              placeholder="First Name"
-              name="firstName"
-              onChange={handleChange}
-              className="p-2 rounded-xl border border-lightBue h-10 text-lg text-black"
-            />
-            <input
-              type="text"
-              placeholder="Last Name"
-              name="lastName"
-              onChange={handleChange}
-              className="p-2 rounded-xl border border-lightBue h-10 text-lg text-black"
-            />
-          </div>
-          <input
-            type="email"
-            required
-            placeholder="Email Adress"
-            className="p-2 rounded-xl border border-lightBue h-10 text-lg text-black"
-            name="email"
-            onChange={handleChange}
-          />
-
-          <div className="flex justify-center lg:justify-start">
-            <button
-              type="submit"
-              className="bg-cyan w-full xl:w-1/2 py-3 px-6 sm:px-10 text-white shadow-md rounded-md"
-            >
-              Finish my purchase
-            </button>
-          </div>
-        </form>
+        <ClientForm />
       </div>
     </main>
   );
---
+++ components/cart/ClientForm.jsx
@@ -0,0 +1,119 @@
+"use client";
+import { db } from "@/firebase/config";
+import { Timestamp, doc, getDoc, setDoc, writeBatch } from "firebase/firestore";
+import { useCartContext } from "../context/CartContext";
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+
+const createOrder = async (values, items) => {
+  //   const docsPromises = items.map((id) => {
+  //     const docRef = doc(db, "products", id.toString());
+  //     return getDoc(docRef);
+  //   });
+
+  //   const docs = await Promise.all(docsPromises);
+  //   const batch = writeBatch(db);
+  //   const outOfStock = [];
+
+  //   docs.forEach((doc) => {
+  //       const { inStock } = doc.data();
+  //       console.log(inStock);
+  //     const itemInCart = items.find((item) => item.id === doc.id);
+
+  //     if (itemInCart.quantity >= inStock) {
+  //       batch.update(doc.ref, { inStock: inStock - itemInCart.quantity });
+  //     } else {
+  //       outOfStock.push(itemInCart);
+  //     }
+  //   });
+
+  //   if (outOfStock.length > 0) return outOfStock;
+
+  const order = {
+    client: values,
+    items: items.map((item) => ({
+      title: item.title,
+      price: item.price,
+      id: item.id,
+      quantity: item.quantity,
+    })),
+    date: new Date().toISOString(),
+  };
+
+  const docId = Timestamp.fromDate(new Date()).toMillis();
+  const orderRef = doc(db, "orders", String(docId));
+  //   await batch.commit();
+  await setDoc(orderRef, order);
+
+  return docId;
+};
+
+const ClientForm = () => {
+  const { cart } = useCartContext();
+  const [values, setValues] = useState({
+    email: "",
+    adress: "",
+    name: "",
+  });
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await createOrder(values, cart);
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      iconColor: "#457b9d",
+      title: "Purchase done!",
+      text: `Order id: ${result}`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    console.log(result);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="grid gap-4 xl:w-1/2 my-6 pb-4">
+      <input
+        type="text"
+        required
+        placeholder="Your name"
+        className="p-2 rounded-xl border border-lightBue h-10 text-lg text-black"
+        name="name"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        required
+        placeholder="Your adress"
+        className="p-2 rounded-xl border border-lightBue h-10 text-lg text-black"
+        name="adress"
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        required
+        placeholder="Your email"
+        className="p-2 rounded-xl border border-lightBue h-10 text-lg text-black"
+        name="email"
+        onChange={handleChange}
+      />
+
+      <button
+        type="submit"
+        className="bg-cyan py-3 px-6 sm:px-10 text-white shadow-md"
+      >
+        Finish my purchase
+      </button>
+    </form>
+  );
+};
+
+export default ClientForm;
